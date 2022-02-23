fa3871d75bfb0aa3368ab9bcb957d2c59de88171
Louis Royer <55180044+louisroyer@users.noreply.github.com>
2022-02-23 10:13:09 +0100

    Add license MIT

--- pfcp_server_entity.go
+++ pfcp_server_entity.go
@@ -1,3 +1,8 @@
+// Copyright 2022 Louis Royer and the go-pfcp-networking contributors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+// SPDX-License-Identifier: MIT
+
 package pfcp_networking
 
 import (
